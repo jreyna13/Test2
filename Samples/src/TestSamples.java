d82a09690ea30f5714a3fb33ef2a3ef55ab07954
jreyna13 <jreyna13@mail.stmarytx.edu>
2015-01-29 23:03:50 -0600

    Initial checkin of Test2.

---
+++ Samples/src/TestSamples.java
@@ -0,0 +1,93 @@
+//Javier Reyna EG1305A Test
+
+import javax.swing.JOptionPane;
+//import java.util.Scanner;
+
+public class TestSamples {
+
+	public static void main(String[] args) {
+		
+		String input = JOptionPane.showInputDialog ("Please enter a radius");
+		
+		int radius = Integer.parseInt(input);
+		
+		JOptionPane.showMessageDialog(null, radius);
+		
+		//System.out.println();
+
+		//double c = 4364.557845;
+
+		//System.out.println(c);
+
+		//System.out.println("\\\'Test\\\'");
+
+		//char z = 'z';
+		//System.out.println(z);
+
+		//int radius = 10;
+		//double area = radius * radius * Math.PI;
+
+		//System.out.printf("\t \"The area of a circle that has radius of %d is %f\"\n", radius, area);
+		
+		//System.out.println();
+		//int a = 5;
+		//int b = 8;
+		//System.out.println(a + "" + b);
+		
+		//comments are done like this or 
+		/* this is for very lonnnnnnng commentssssssssssssssssssssssssss 
+		 * and next line return*/
+		
+		//Scanner input = new Scanner(System.in);
+		
+		//System.out.println("Please enter name");
+		
+		//String name = input.nextLine();
+		
+		//int Radio = input.nextInt();
+		
+		//double areas = radius * radius *Math.PI;
+		//System.out.println(name);
+		
+		/*
+		private double customers;
+	    private double thoseThatPurch;
+	    private double preferCitrus;
+	    private double nopeople1;
+	    private double nopeople2;
+	    
+
+
+	public SoftDrink()
+	{
+	    customers = 12467;
+	    thoseThatPurch = .14;
+	    preferCitrus = .64;
+	}
+
+	    
+	    public void getInformation(double customers, double thoseThatPurch, double     preferCitrus)
+	    {
+	    }
+
+
+	    public void printOutput()
+	    {
+	        nopeople1 = customers * thoseThatPurch;
+	        nopeople2 = nopeople1 * preferCitrus;
+
+	        System.out.println("********************************************");
+	        System.out.println("ENERGY DRINK CONSUMPTION");
+	        System.out.println("********************************************");
+	        System.out.println("Number of people surveyed: " + customers);
+	        System.out.println("Number who purchase one or more energy drinks: " +  nopeople1);
+	        System.out.println("Number who prefer citrus flavored energy drinks: " + nopeople2);
+	        System.out.println();
+	        */
+
+	    
+		
+		System.exit(0);
+		
+	}
+}
